97935d237ef5a8ff2feade2ce2a2a2db82551bae
Dale Sande <dale.sande@gmail.com>
2015-09-15 11:51:44 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+// Gulp Sass Task
+gulp.task('build:css', function() {
+  gulp.src('./scss/{,*/}*.{scss,sass}')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function() {
+  gulp.watch('./scss/{,*/}*.{scss,sass}', ['build:css'])
+})
+
+gulp.task('default', ['build:css']);
